0896a3f39b85189708897a5991f4089019411c19
Utkrisht Agrawal <rkayush412@gmail.com>
2025-12-27 11:14:57 +0530

    Vectore Store Updated

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -4,7 +4,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 def create_vector_store(documents):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"}  # 🔥 CRITICAL FIX
+        model_kwargs={"device": "cpu"}
     )
 
     return FAISS.from_documents(documents, embeddings)
